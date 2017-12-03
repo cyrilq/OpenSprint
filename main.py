b1a4b266605915d505b3930d66540c07fdf220cd
cyrilq <kuharchuk@styleru.net>
2017-12-03 11:02:49 +0300

    polling added

--- main.py
+++ main.py
@@ -8,3 +8,28 @@ import logging
 import html
 from config import API_TOKEN
 from consts import START_MESSAGE
+
+
+bot = telebot.TeleBot(API_TOKEN)
+
+logging.basicConfig(level=logging.INFO)
+
+@bot.message_handler(commands=['start', 'help'])
+def send(message):
+    bot.send_message(message.chat.id, text=START_MESSAGE)
+
+
+@bot.message_handler(commands=['info'])
+def send(message):
+    pass
+
+
+# polling cycle
+if __name__ == '__main__':
+    while True:
+        logging.info('The polling cycle has started!')
+        try:
+            bot.polling(none_stop=True)
+        except requests.exceptions.ConnectionError as e:
+            logging.warning('There was requests.exceptions.ConnectionError')
+            time.sleep(15)
