feda238682fd9e7403ca9e334a6bcc35a5a3e6bb
cyrilq <kuharchuk@styleru.net>
2017-12-03 10:52:50 +0300

    config and consts files added

---
+++ main.py
@@ -0,0 +1,10 @@
+import telebot
+from telebot import types
+import re
+import requests
+import time
+import json
+import logging
+import html
+from config import API_TOKEN
+from consts import START_MESSAGE
