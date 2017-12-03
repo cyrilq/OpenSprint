00ec07a37f5f91b02ad95fd2185e6181c094d82c
cyrilq <kuharchuk@styleru.net>
2017-12-03 11:53:53 +0300

    info command added

--- main.py
+++ main.py
@@ -19,9 +19,29 @@ def send(message):
     bot.send_message(message.chat.id, text=START_MESSAGE)
 
 
-@bot.message_handler(commands=['info'])
+@bot.message_handler(commands=['inn'])
 def send(message):
-    pass
+    if message.text[5:] == "":
+        bot.send_message(message.chat.id, text="Проверьте правильность ввода")
+    else:
+        if True:
+            nko_info = ("\n"
+                        ""
+                        "Есть ли у НКО Издание/вещательный канал: {}\n"
+                        "Ссылка на карточку организации на портале Открытые НКО: {}\n"
+                        "Лицензии на СМИ: {}\n"
+                        "ИНН и ОГРН этой НКО: {}\n"
+                        "Тип организации: {}\n"
+                        "Статус организации (действующая/исключена): {}\n"
+                        "Регион: {}\n"
+                        "Территория распространения СМИ: {}\n"
+                        "Язык(и) СМИ: {}\n"
+                        "Форма выпуска: {}\n"
+                        "Адрес сайта: {}\n"
+                        "Ссылка на карточку СМИ на портале Открытые СМИ: {}\n"
+                        "Перечень соучредителей, если таковые есть: {}\n"
+                        "                ")
+        pass
 
 
 # polling cycle
